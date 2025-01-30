e6c4679d36e9d9a68a2c7ce06d36bfa930a7af73
Christian G <cgrab713@gmail.com>
2025-01-30 20:31:22 +0000

    use hash routing

--- src/other/PageRoutes.js
+++ src/other/PageRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from './NotFound';
 import App from '../App';
 import PhotoHome from '../photography/PhotoHome';
@@ -9,8 +9,8 @@ const PageRoutes = () => {
     <Router>
       <Routes>
         {/* Fallback route for 404 */}
-        <Route path='/' element={<App/>} />
-        <Route path="/photo" element={<PhotoHome/>} />
+        <Route path='/' exact element={<App />} />
+        <Route path="/photo" exact element={<PhotoHome />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
